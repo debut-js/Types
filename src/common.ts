d878bbe98bcd8990e59d04aa272de3bf78fe7c7a
Yurov Dmitry <dmitry.yurov@corp.mail.ru>
2021-06-27 13:31:28 +0300

    fix type of ticks handler is async now

--- src/common.ts
+++ src/common.ts
@@ -6,7 +6,7 @@ import { Candle } from './candle';
 export type TimeFrame = '1min' | '3min' | '5min' | '15min' | '30min' | '1h' | '2h' | '4h' | 'day' | 'week' | 'month';
 
 /** Handler for tick or candle change */
-export type TickHandler = (tick: Candle) => void;
+export type TickHandler = (tick: Candle) => Promise<void>;
 
 /**
  * Debut environment values
