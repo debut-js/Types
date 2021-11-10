0f3605aa74300cd0495fe56bbf84ec74a58fdc83
Yurov Dmitry <dmitry.yurov@corp.mail.ru>
2021-11-10 16:34:27 +0300

    order options overhead removed

--- src/order.ts
+++ src/order.ts
@@ -38,30 +38,6 @@ interface BaseOrder {
 }
 
 /**
- * Debut order parameters
- */
-export interface OrderOptions {
-    // Current broker, supported "Binance", "Tinkoff"
-    broker: string;
-    // Ticker name
-    ticker: string;
-    // Broker asset id (if exists)
-    figi?: string;
-    // Currency name
-    currency: string;
-    // Candle time frame
-    interval: TimeFrame;
-    // Lots will be multiplied to this value (preferred for martingale systems)
-    lotsMultiplier?: number;
-    // How many equity you want use from balance. In percent values between 0 and 1, e.g. 0.97 = 97%
-    equityLevel?: number;
-    // Order Instrument type
-    instrumentType: InstrumentType;
-    // Retry counts
-    retries?: number;
-}
-
-/**
  * Pending order data
  */
 export interface PendingOrder extends BaseOrder {
@@ -74,7 +50,7 @@ export interface PendingOrder extends BaseOrder {
 /**
  * Executed order data
  */
-export interface ExecutedOrder extends PendingOrder, OrderOptions {
+export interface ExecutedOrder extends PendingOrder {
     // Placed order identifier from server
     orderId: string;
     // How many lots are filled. May be not equal with lots field if order have partial fill
